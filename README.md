85700eb02a7c401f7086e161343d12c60b8c0c1e
Wu Yu-Chen <winie850609@gmail.com>
2017-12-19 01:06:06 +0800

    Start to implement the program including threads and mutexs.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # threadTest
 Practice thread and mutex in C++11
+
+We must include two libraries which are <thread> and <mutex>.
+Then, the compile command is: "g++ -std=c++11 <Filename>"
+
+After execute the program, you can see the executing order between each threads.
---
+++ testThread.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+using namespace std;
+
+mutex mMutex;
+mutex pMutex;
+
+void print(int num){
+	pMutex.lock();
+	cout<<"Thread1 No."<<num-1<<endl;
+	pMutex.unlock();
+}
+
+void test(int num){
+	mMutex.lock();
+	int sum = 0;
+	for(int i=0;i<num;i++)
+		sum+=i;
+	cout<<"Thread No."<<num-1<<endl;
+	cout<<"sum: "<<sum<<endl;
+	mMutex.unlock();
+	print(num);
+}
+
+int main( int argc, char** argv )
+{
+	thread threadArr[10];
+	
+	for(int i=1;i<=10;i++)
+	{
+		if(i<5)
+			threadArr[i-1] = thread(test, i);
+		else
+			threadArr[i-1] = thread(print, i);
+	}
+	for(int i=1;i<=10;i++)
+	{
+		threadArr[i-1].join();
+	}
+	cout<<"Done."<<endl;
+	return 0;
+}
