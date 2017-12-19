65e5d80525b9d7991a285ff96823d1e36c545db2
Wu Yu-Chen <winie850609@gmail.com>
2017-12-19 16:38:28 +0800

    Modify the formats of the readme and source file.

--- testThread.cpp
+++ testThread.cpp
@@ -24,19 +24,16 @@ void test(int num){
 	print(num);
 }
 
-int main( int argc, char** argv )
-{
+int main( int argc, char** argv ){
 	thread threadArr[10];
 	
-	for(int i=1;i<=10;i++)
-	{
+	for(int i=1;i<=10;i++){
 		if(i<5)
 			threadArr[i-1] = thread(test, i);
 		else
 			threadArr[i-1] = thread(print, i);
 	}
-	for(int i=1;i<=10;i++)
-	{
+	for(int i=1;i<=10;i++){
 		threadArr[i-1].join();
 	}
 	cout<<"Done."<<endl;
